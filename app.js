c20a896ac201b23e7f9fba7a782228d5f9bd9779
unknown <you@example.com>
2021-06-15 17:33:18 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const app = express();
+const swaggerOptions = {
+	swaggerDefinition: {
+	info: {
+		title: 'Library API',
+		version: '1.0.0'
+	}
+	},
+	apis: ['app.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+//console.log(swaggerDocs);
+
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *    description: Get all books
+ *    responses:
+ *     200:
+ *      description: Success
+ *
+ */
+
+app.get('/books', (req, res) => {
+	res.send([
+		{
+			id:1,
+			title: 'title'
+	        }
+	]);
+ 
+});
+
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *    description: Create a new book
+ *    parameters:
+ *      - name: title
+ *        description: title of the book
+ *        in: formData
+ *        required: true
+ *        type: string
+ *    responses:
+ *      201:
+ *
+ */
+ 
+app.post('/boots', (req,res) => {
+	res.status(201).send();
+})
+
+app.listen(3000, () => console.log("Listening on port 3000"));
